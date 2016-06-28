5c896b6488168c0fe0a3908c4ff8ea697c151d8a
Tyler.Yin <yinzhiyongyzy@126.com>
2016-06-29 00:08:18 +0800

    创建easyPost工程

---
+++ easyPost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  easyPost
+//
+//  Created by Tyler.Yin on 16/6/29.
+//  Copyright © 2016年 ayong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
